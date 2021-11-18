08401428858107c2ab5a22ef61a1440f7556c7c9
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2021-11-18 14:56:11 +0000

    Accept user commands list all or list name

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -1,51 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
-using System.Linq;
 
 namespace SupportBank
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            List<Transaction> transactions = new List<Transaction>();
+            var transactions = new List<Transaction>();
+            var balances = new Dictionary<string, decimal>();
             using (var reader =
                 new StreamReader(@"C:\Work\Training\SupportBank\SupportBank-master\Transactions2014.csv"))
             {
-                string headerLine = reader.ReadLine();
+                var headerLine = reader.ReadLine();
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    string[] values = line.Split(',');
-                    Transaction newTransaction = new Transaction();
+                    var values = line.Split(',');
+                    var newTransaction = new Transaction();
                     newTransaction.date = values[0];
                     newTransaction.to = values[1];
                     newTransaction.from = values[2];
                     newTransaction.narrative = values[3];
                     newTransaction.amount = Convert.ToDecimal(values[4]);
-                    
-                    transactions.Add(newTransaction);
-                }
 
-                foreach (var transaction in transactions)
-                {
-                    Console.WriteLine(transaction.date);
-                    Console.WriteLine(transaction.from);
-                    Console.WriteLine(transaction.to);
-                    Console.WriteLine(transaction.narrative);
-                    Console.WriteLine(transaction.amount);
+                    transactions.Add(newTransaction);
+
+                    if (balances.ContainsKey(newTransaction.to))
+                        balances[newTransaction.to] += newTransaction.amount;
+                    else
+                        balances[newTransaction.to] = newTransaction.amount;
+                    if (balances.ContainsKey(newTransaction.from))
+                        balances[newTransaction.from] -= newTransaction.amount;
+                    else
+                        balances[newTransaction.from] = -1 * newTransaction.amount;
                 }
             }
-        }
-    }
 
-    class Transaction
-    {
-        public string date;
-        public string from;
-        public string to;
-        public string narrative;
-        public decimal amount;
+            var command = Console.ReadLine().ToLower();
+            if (command == "list all")
+            {
+                Console.WriteLine("Here are the current net balances:");
+                foreach (var item in balances)
+                {
+                    var netPhrase = "";
+                    decimal netAmount = 0;
+                    switch (item.Value)
+                    {
+                        case < 0:
+                            netPhrase = "owes";
+                            netAmount = item.Value * -1;
+                            break;
+                        default:
+                            netPhrase = "is owed";
+                            netAmount = item.Value;
+                            break;
+                    }
+
+                    Console.WriteLine(string.Join(" ", item.Key, netPhrase, "£" + netAmount));
+                }
+            }
+            else if (command.Substring(0, 5) == "list ")
+            {
+                var account = command.Substring(5);
+                var myTI = new CultureInfo("en-UK", false).TextInfo;
+                Console.WriteLine("Here are all the transactions associated with " + myTI.ToTitleCase(account) + ":");
+                foreach (var transaction in transactions)
+                    if ((transaction.from.ToLower() == account) | (transaction.to.ToLower() == account))
+                        Console.WriteLine(string.Join(" ", "On", transaction.date + ",", transaction.from, "paid",
+                            transaction.to, "£" + transaction.amount, "for", transaction.narrative.ToLower()));
+            }
+        }
     }
 }
---
+++ SupportBank/Transaction.cs
@@ -0,0 +1,16 @@
+﻿namespace SupportBank
+{
+    public class Transaction
+    {
+        public decimal amount;
+        public string date;
+        public string from;
+        public string narrative;
+        public string to;
+
+        public override string ToString()
+        {
+            return string.Join(" ", date, from, to, narrative, amount.ToString());
+        }
+    }
+}
