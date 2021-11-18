6a630cebe06399b37bcdc7769d0207024d5b148b
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2021-11-18 15:34:43 +0000

    Log the line being read at the point of an error

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -2,40 +2,64 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+
 
 namespace SupportBank
 {
     internal class Program
     {
+        
+        private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
         private static void Main(string[] args)
         {
+            var config = new LoggingConfiguration();
+            var target = new FileTarget { FileName = @"C:\Work\Logs\SupportBank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}" };
+            config.AddTarget("File Logger", target);
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
+            LogManager.Configuration = config;
+            
             var transactions = new List<Transaction>();
             var balances = new Dictionary<string, decimal>();
             using (var reader =
-                new StreamReader(@"C:\Work\Training\SupportBank\SupportBank-master\Transactions2014.csv"))
+                new StreamReader(@"C:\Work\Training\SupportBank\SupportBank-master\DodgyTransactions2015.csv"))
             {
                 var headerLine = reader.ReadLine();
                 string line;
+                int lineCount = 2;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    var values = line.Split(',');
-                    var newTransaction = new Transaction();
-                    newTransaction.date = values[0];
-                    newTransaction.to = values[1];
-                    newTransaction.from = values[2];
-                    newTransaction.narrative = values[3];
-                    newTransaction.amount = Convert.ToDecimal(values[4]);
+                    try
+                    {
+                        var values = line.Split(',');
+                        var newTransaction = new Transaction();
+                        newTransaction.date = values[0];
+                        newTransaction.to = values[1];
+                        newTransaction.from = values[2];
+                        newTransaction.narrative = values[3];
+                        newTransaction.amount = Convert.ToDecimal(values[4]);
 
-                    transactions.Add(newTransaction);
+                        transactions.Add(newTransaction);
 
-                    if (balances.ContainsKey(newTransaction.to))
-                        balances[newTransaction.to] += newTransaction.amount;
-                    else
-                        balances[newTransaction.to] = newTransaction.amount;
-                    if (balances.ContainsKey(newTransaction.from))
-                        balances[newTransaction.from] -= newTransaction.amount;
-                    else
-                        balances[newTransaction.from] = -1 * newTransaction.amount;
+                        if (balances.ContainsKey(newTransaction.to))
+                            balances[newTransaction.to] += newTransaction.amount;
+                        else
+                            balances[newTransaction.to] = newTransaction.amount;
+                        if (balances.ContainsKey(newTransaction.from))
+                            balances[newTransaction.from] -= newTransaction.amount;
+                        else
+                            balances[newTransaction.from] = -1 * newTransaction.amount;
+                    }
+                    catch
+                    {
+                        logger.Debug("Error storing line " + lineCount + " as a transaction");
+                    }
+
+                    lineCount++;
+
                 }
             }
 
