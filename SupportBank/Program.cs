d8039f64ce148b1225452af72ad17bac7cc4875a
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2021-11-18 11:20:36 +0000

    Save data as list of transaction objects

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace SupportBank
 {
@@ -7,15 +9,43 @@ namespace SupportBank
     {
         static void Main(string[] args)
         {
+            List<Transaction> transactions = new List<Transaction>();
             using (var reader =
                 new StreamReader(@"C:\Work\Training\SupportBank\SupportBank-master\Transactions2014.csv"))
             {
+                string headerLine = reader.ReadLine();
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    Console.WriteLine(line);
+                    string[] values = line.Split(',');
+                    Transaction newTransaction = new Transaction();
+                    newTransaction.date = values[0];
+                    newTransaction.to = values[1];
+                    newTransaction.from = values[2];
+                    newTransaction.narrative = values[3];
+                    newTransaction.amount = Convert.ToDecimal(values[4]);
+                    
+                    transactions.Add(newTransaction);
+                }
+
+                foreach (var transaction in transactions)
+                {
+                    Console.WriteLine(transaction.date);
+                    Console.WriteLine(transaction.from);
+                    Console.WriteLine(transaction.to);
+                    Console.WriteLine(transaction.narrative);
+                    Console.WriteLine(transaction.amount);
                 }
             }
         }
+    }
+
+    class Transaction
+    {
+        public string date;
+        public string from;
+        public string to;
+        public string narrative;
+        public decimal amount;
     }
 }
